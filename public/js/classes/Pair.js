07d028d93f524bb5417c5da0fd43f6d2dcfa07cb
Carmen Vavra <carmen070976@gmail.com>
2022-04-18 19:50:16 +0200

    pairs

--- public/js/classes/Pair.js
+++ public/js/classes/Pair.js
@@ -1,6 +1,6 @@
 class Pair {
   constructor(card1, card2, id1, id2) {
-    console.log('card1', card1, 'card2', card2);
+    // console.log('card1', card1, 'card2', card2, 'id1', id1, 'id2', id2);
     this.card1 = card1;
     this.card2 = card2;
     this.id1 = id1;
@@ -32,47 +32,49 @@ class Pair {
    * 
    * @returns true|false
    */
-  compareCards(vocabualaryArray, foreignArray) {
+  compareCards(vocabularyArray, foreignArray) {
     let indexCard1;
     let indexCard2;
 
-    console.log('vocabualaryArray', vocabualaryArray, 'foreignArray', foreignArray);
-    if(vocabualaryArray.includes(this.card1)){
-      indexCard1 = vocabualaryArray.indexOf(this.card1);
+    // console.log('vocabularyArray', vocabularyArray, 'foreignArray', foreignArray);
+    if(vocabularyArray.includes(this.card1)){
+      indexCard1 = vocabularyArray.indexOf(this.card1);
+      vocabularyArray.splice(indexCard1, 1);
     }
 
     if(foreignArray.includes(this.card2)){
       indexCard2 = foreignArray.indexOf(this.card2);
+      foreignArray.splice(indexCard2, 1);
     }
 
-    console.log('indexCard1', indexCard1, 'indexCard2', indexCard2);
-
-
-
+    // console.log('indexCard1', indexCard1, 'indexCard2', indexCard2);
+    if (this.id1 !== this.id2) {
+        if (indexCard1 == indexCard2) {
+          return true;
+        } else {
+          return false;
+        }
+    }
     // OLD VERSION
 
-    if (this.id1 !== this.id2) {
+    
       /* console.log('this.card1', this.card1, 'this.card2', this.card2); */
-
-      if (vocabualaryArray.includes(this.card1)) {
-        indexCard1 = vocabualaryArray.indexOf(this.card1);
+/* 
+      if (vocabularyArray.includes(this.card1)) {
+        indexCard1 = vocabularyArray.indexOf(this.card1);
       } else if (foreignArray.includes(this.card1)) {
         indexCard1 = foreignArray.indexOf(this.card1);
       }
 
-      if (vocabualaryArray.includes(this.card2)) {
-        indexCard2 = vocabualaryArray.indexOf(this.card2);
+      if (vocabularyArray.includes(this.card2)) {
+        indexCard2 = vocabularyArray.indexOf(this.card2);
       } else if (foreignArray.includes(this.card2)) {
         indexCard2 = foreignArray.indexOf(this.card2);
       }
+ */
 
-      if (indexCard1 == indexCard2) {
-        return true;
-      } else {
-        return false;
-      }
 
-    }
+   
   }
 
 }
